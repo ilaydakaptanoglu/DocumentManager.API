f2228f066074471a6301308b2763f0b28a2659fd
İlayda KAPTANPĞLU <116031869+ilaydakaptanoglu@users.noreply.github.com>
2025-09-02 15:05:38 +0300

    son migration yapıldı

---
+++ Migrations/20250902111857_Admin-Name-Surname.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DocumentManager.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminNameSurname : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
